9bf697d25979fa1721665b8ab87e700126d4016c
Nao Sasaki <lanberb.2@gmail.com>
2021-03-20 19:06:29 +0900

    change: manifest icon

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -7,7 +7,7 @@
         </div>
         <div class="studioList">
             <nuxt-link class="studio-card"
-            v-for="s in studioList"
+            v-for="(s, index) in studioList"
             :key="s.id"
             :to="{
                 name: 'studio-id',
@@ -16,7 +16,7 @@
                     id: s.id,
                 },
             }">
-                <img src="~/static/caught-in-joy-ptVBlniJi50-unsplash.jpg" alt="studio-img">
+                <img :src="thumbnailList[index]" alt="studio-img">
                 <div class="card-favorite">
                     <svg viewBox="0 0 24 24">
                         <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" stroke="#fff" stroke-width="1.6px"/>
@@ -45,24 +45,41 @@ export default {
     data: function() {
         return {
             studioList: '',
+            thumbnailList: '',
+            url: '',
         }
     },
     asyncData: async function() {
         const db = firebase.firestore();
         var list = [];
-        return db.collection('studio').get().then(function(querySnapshot){
+        var thumbnails = [];
+        await db.collection('studio').get().then(function(querySnapshot){
             querySnapshot.forEach((doc) => {
                 const studio = doc.data();
                 studio.id = doc.id;
                 list.push(studio);
             });
-            return {
-                studioList: list.concat(),
-            }
         }).catch(console.error);
+
+        const urls = list.map(i => i.thumbnail);
+        for (let i = 0; i < list.length; i++) {
+            if (!!urls[i]) {
+                const url = await firebase.storage().ref().child('studio-thumbnails/' + urls[i]);
+                await url.getDownloadURL().then(function(res) {
+                    thumbnails.push(res);
+                });
+            }
+            if (!urls[i]) thumbnails.push(null);
+        }
+        console.log(thumbnails);
+        return {
+            studioList: list.concat(),
+            thumbnailList: thumbnails.concat(),
+        }
     },
     mounted: function() {
         if (!(!!firebase.auth().currentUser)) this.$router.push('/');
+        console.log(this.url)
     },
 }
 </script>
